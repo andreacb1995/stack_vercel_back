a385d37010f3a5bb48968fe7b6832a2962dbce15
andreacb1995 <andrea.carballido.ballesteros@gmail.com>
2025-01-17 18:16:03 +0100

    first commit

--- api/index.js
+++ api/index.js
@@ -30,13 +30,6 @@
     })
   );
 
-  // Sirve archivos estáticos desde la carpeta "public"
-  app.use(express.static(path.join(__dirname, '../public')));
-
-  // Ruta principal para servir index.html
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
 
   // Ruta para /api
   app.get('/api', (req, res) => {
